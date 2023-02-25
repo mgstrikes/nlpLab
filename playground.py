b34b26599edc89a5cbeeca19477297119275cb88
sebastianriya91 <sebastianriya91@gmail.com>
2023-02-25 22:26:10 +0000

    spacy start

---
+++ playground.py
@@ -0,0 +1,11 @@
+name = 'test'
+print(f'my name ios {name}')
+
+d = {''}
+
+myFile = open('test.txt')
+print(myFile.read())
+myFile.seek(0)
+print(myFile.read())
+myFile.close()
+myFile.writelines()
